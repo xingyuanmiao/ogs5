dc669430b8443535e1082ecdd4045db828fa074d
tnagel <thomas.nagel@ufz.de>
2016-06-17 14:54:50 +0200

    Removed redundant operations from Burgers model.

--- FEM/burgers.cpp
+++ FEM/burgers.cpp
@@ -37,7 +37,7 @@ SolidBurgers::~SolidBurgers()
    Programing:
    07/2014 TN Implementation
 **************************************************************************/
-void SolidBurgers::UpdateBurgersProperties(double s_eff, const double Temperature)
+void SolidBurgers::UpdateBurgersProperties(const double s_eff, const double Temperature)
 {
 	double dT = Temperature - T_ref;
 	GM = GM0 + m_GM*dT;
@@ -57,19 +57,14 @@ void SolidBurgers::CalResidualBurgers(const double dt, const Eigen::Matrix<doubl
                                       Eigen::Matrix<double,6,1> &strain_Kel_curr, const Eigen::Matrix<double,6,1> &strain_Kel_t,
                                       Eigen::Matrix<double,6,1> &strain_Max_curr, const Eigen::Matrix<double,6,1> &strain_Max_t, Eigen::Matrix<double,18,1> &res)
 {
-    Eigen::Matrix<double,6,1> G_j;
-
     //calculate stress residual
-    G_j = stress_curr - 2. * (strain_curr - strain_Kel_curr - strain_Max_curr);
-    res.block<6,1>(0,0) = G_j;
+    res.block<6,1>(0,0) = stress_curr - 2. * (strain_curr - strain_Kel_curr - strain_Max_curr);
 
     //calculate Kelvin strain residual
-    G_j = 1./dt * (strain_Kel_curr - strain_Kel_t) - 1./(2.*etaK) * (GM*stress_curr - 2.*GK*strain_Kel_curr);
-    res.block<6,1>(6,0) = G_j;
+    res.block<6,1>(6,0) = 1./dt * (strain_Kel_curr - strain_Kel_t) - 1./(2.*etaK) * (GM*stress_curr - 2.*GK*strain_Kel_curr);
 
     //calculate Maxwell strain residual
-    G_j = 1./dt * (strain_Max_curr - strain_Max_t) - GM/(2.*etaM)*stress_curr;
-    res.block<6,1>(12,0) = G_j;
+    res.block<6,1>(12,0) = 1./dt * (strain_Max_curr - strain_Max_t) - GM/(2.*etaM)*stress_curr;
 
 
 }
@@ -80,21 +75,8 @@ void SolidBurgers::CalResidualBurgers(const double dt, const Eigen::Matrix<doubl
    Programing:
    06/2014 TN Implementation
 **************************************************************************/
-void SolidBurgers::CalJacobianBurgers(const double dt, Eigen::Matrix<double,18,18> &Jac, double s_eff, const Eigen::Matrix<double,6,1> &sig_i, const Eigen::Matrix<double,6,1> &eps_K_i)
+void SolidBurgers::CalJacobianBurgers(const double dt, Eigen::Matrix<double,18,18> &Jac, const double s_eff, const Eigen::Matrix<double,6,1> &sig_i, const Eigen::Matrix<double,6,1> &eps_K_i)
 {
-    //6x6 submatrices of the Jacobian
-    Eigen::Matrix<double,6,6> G_ij;
-    //avoid division by 0
-    s_eff = std::max(s_eff,DBL_EPSILON);
-
-    //aid terms
-    Eigen::Matrix<double,6,1> eps_K_aid, dG_K, dmu_vK, dmu_vM;
-
-    eps_K_aid = 1./(etaK*etaK)*(GM*sig_i-2.*GK*eps_K_i);
-    dG_K = mK * 3.*GK*GM/(2.*s_eff)*sig_i;
-    dmu_vK = mvK * 3.*GM*etaK/(2.*s_eff)*sig_i;
-    dmu_vM = mvM * 3.*GM*etaM/(2.*s_eff)*sig_i;
-
     //Check Dimension of Jacobian
     if (Jac.cols() != 18 || Jac.rows() != 18)
     {
@@ -104,36 +86,43 @@ void SolidBurgers::CalJacobianBurgers(const double dt, Eigen::Matrix<double,18,1
     Jac.setZero(18,18);
 
     //build G_11
-    G_ij = smath->ident;
-    Jac.block<6,6>(0,0) = G_ij;
+    Jac.block<6,6>(0,0) = smath->ident;
 
     //build G_12
-    G_ij = 2. * smath->ident;
-    Jac.block<6,6>(0,6) = G_ij;
+    Jac.block<6,6>(0,6) = 2. * smath->ident;
 
     //build G_13
-    G_ij = 2. * smath->ident;
-    Jac.block<6,6>(0,12) = G_ij;
+    Jac.block<6,6>(0,12) = 2. * smath->ident;
 
     //build G_21
-    G_ij = -GM/(2.*etaK) * smath->ident + 0.5*eps_K_aid*dmu_vK.transpose() + 1./etaK * eps_K_i * dG_K.transpose();
-    Jac.block<6,6>(6,0) = G_ij;
+    Jac.block<6,6>(6,0) = -GM/(2.*etaK) * smath->ident;
 
     //build G_22
-    G_ij = (1./dt + GK/etaK) * smath->ident;
-    Jac.block<6,6>(6,6) = G_ij;
+    Jac.block<6,6>(6,6) = (1./dt + GK/etaK) * smath->ident;
 
     //nothing to do for G_23
 
     //build G_31
-    G_ij = - GM/(2.*etaM)* smath->ident + GM/(2.*etaM*etaM) * sig_i * dmu_vM.transpose();
-    Jac.block<6,6>(12,0) = G_ij;
+    Jac.block<6,6>(12,0) = - GM/(2.*etaM)* smath->ident;
 
     //nothing to do for G_32
 
     //build G_33
-    G_ij = 1./dt * smath->ident;
-    Jac.block<6,6>(12,12) = G_ij;
+    Jac.block<6,6>(12,12) = 1./dt * smath->ident;
+
+    if (s_eff > 0.)
+    {
+        const Eigen::Matrix<double,6,1> dG_K = mK * 3.*GK*GM/(2.*s_eff)*sig_i;
+        const Eigen::Matrix<double,6,1> dmu_vK = mvK * 3.*GM*etaK/(2.*s_eff)*sig_i;
+        const Eigen::Matrix<double,6,1> dmu_vM = mvM * 3.*GM*etaM/(2.*s_eff)*sig_i;
+        const Eigen::Matrix<double,6,1> eps_K_aid = 1./(etaK*etaK)*(GM*sig_i-2.*GK*eps_K_i);
+
+        //build G_21
+        Jac.block<6,6>(6,0) += 0.5*eps_K_aid*dmu_vK.transpose() + 1./etaK * eps_K_i * dG_K.transpose();
+
+        //build G_31
+        Jac.block<6,6>(12,0) += GM/(2.*etaM*etaM) * sig_i * dmu_vM.transpose();
+    }
 }
 
 /**************************************************************************
@@ -145,12 +134,6 @@ void SolidBurgers::CalJacobianBurgers(const double dt, Eigen::Matrix<double,18,1
 void SolidBurgers::CaldGdEBurgers(Eigen::Matrix<double,18,6> &dGdE)
 {
 
-    //6x6 submatrices of the Jacobian
-    //Eigen::Matrix<double, 6, 6> dGdE_1(1./dt *D_Max);
-    Eigen::Matrix<double, 6, 6> dGdE_1;
-
-    dGdE_1 = -2. * smath->ident;
-
     //Check Dimension of dGdE
     if (dGdE.cols() != 6 || dGdE.rows() != 18)
     {
@@ -159,7 +142,7 @@ void SolidBurgers::CaldGdEBurgers(Eigen::Matrix<double,18,6> &dGdE)
     }
 
     dGdE.setZero(18,6);
-    dGdE.block<6,6>(0,0) = dGdE_1;
+    dGdE.block<6,6>(0,0) = -2. * smath->ident;
 
 }
 
--- FEM/burgers.h
+++ FEM/burgers.h
@@ -35,7 +35,7 @@ public:
                             const Eigen::Matrix<double,6,1> &stress_curr, Eigen::Matrix<double,6,1> &strain_Kel_curr,
                             const Eigen::Matrix<double,6,1> &strain_Kel_t, Eigen::Matrix<double,6,1> &strain_Max_curr,
                             const Eigen::Matrix<double,6,1> &strain_Max_t, Eigen::Matrix<double,18,1> &res);
-    void CalJacobianBurgers(const double dt, Eigen::Matrix<double,18,18> &Jac, double s_eff, const Eigen::Matrix<double,6,1> &sig_i, const Eigen::Matrix<double,6,1> &eps_K_i);
+    void CalJacobianBurgers(const double dt, Eigen::Matrix<double,18,18> &Jac, const double s_eff, const Eigen::Matrix<double,6,1> &sig_i, const Eigen::Matrix<double,6,1> &eps_K_i);
     void CaldGdEBurgers(Eigen::Matrix<double,18,6> &dGdE);
 
     Invariants* smath;
